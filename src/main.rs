b28baf40ef27797c9af8a0ca2b8e6720836b80c5
Sergey Makarichev <harleymack@mail.ru>
2022-10-18 13:26:38 +0300

    array practice

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use rand::{Rng, thread_rng};
-use rand::distributions::{Alphanumeric, DistString};
-
 mod old;
 
+#[allow(unused_variables)]
+#[allow(dead_code)]
+
 fn main() {
-    let mut rng = thread_rng();
-    let a: i32 = rng.gen();
-    println!("{}", a);
+    let primes = [2, 3, 5, 7, 11];
+    println!("{:?}", primes);
 
-    println!("bounded int: {}", rng.gen_range( 0.0..100.0));
+    let mut numbers = [0; 15];
 
-    let rand_string: String = Alphanumeric.sample_string(&mut thread_rng(), 20);
-    println!("{}", rand_string);
+    for number in numbers {
+        println!("{}", number);
+    }
 }
