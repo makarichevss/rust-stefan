473b7b5a043d78dae15b7e3d0c79c18deaa08df8
Sergey Makarichev <harleymack@mail.ru>
2022-10-18 20:19:30 +0300

    struct practice

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,29 @@ mod old;
 #[allow(dead_code)]
 
 fn main() {
-    let mut primes: Vec<i32> = Vec::new();
-    primes.push(12);
-    let mut primes2 = [2, 3, 5, 7, 11];
-    println!("{:?}", primes);
-    println!("{:?}", primes2);
-
-    let mut vect = vec![1; 10];
-    let slice = &mut primes2[1..3];
-    println!("{:?}", slice[1]);
-    println!("{:?}", slice);
-    let mut colors = ["red", "yellow", "blue"];
-    colors[0] = "red";
-    update_colors(&mut colors[1..3]);
-    println!("{:?}", colors);
-
-    let person = ("John", 27, true);
-    let (a, b, _) = person;
-    println!("{:?}", b);
+    let emp = Employee {
+        name: String::from("John"),
+        age: 35
+    };
+    println!("{:#?}", emp);
+    println!("{:#?}", emp.age);
+    println!("{:#?}", emp.details());
+    println!("{:#?}", Employee::static_details());
 }
 
-fn update_colors(color_slice: &mut [&str]) {
-    color_slice[0] = "red";
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Employee {
+    name: String,
+    age: i32
+}
+
+impl Employee {
+    fn details(&self) -> String {
+        format!("name: {}, age: {}", &self.name, &self.age)
+    }
+
+    fn static_details() -> String {
+        String::from("Details static")
+    }
 }
